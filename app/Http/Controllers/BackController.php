dd8b25e231c03ae339a35f7b2d3d7487cf29642e
Fathur Walkers <fathurwalkers44@gmail.com>
2021-07-21 23:24:57 -0700

    update

---
+++ app/Http/Controllers/BackController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Detail;
+use App\Models\Login;
+
+class BackController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $cariUser = Login::where('username', $request->username)->get();
+        if ($cariUser->isEmpty()) {
+            return back()->with('status_fail', 'Maaf username atau password salah!')->withInput();
+        }
+        $data_login = Login::where('username', $request->username)->firstOrFail();
+        switch ($data_login->level) {
+            case 'admin':
+                $cek_password = Hash::check($request->password, $data_login->password);
+                if ($data_login) {
+                    if ($cek_password) {
+                        $users = session(['data_login' => $data_login]);
+                        return redirect()->route('dashboard');
+                    }
+                }
+                break;
+            case 'guru':
+                if ($request->password == $data_login->password) {
+                    $users = session(['data_login' => $data_login]);
+                    return redirect()->route('dashboard');
+                }
+                break;
+            case 'siswa':
+                if ($request->password == $data_login->password) {
+                    $users = session(['data_login' => $data_login]);
+                    return redirect()->route('dashboard');
+                }
+                break;
+            case 'kepsek':
+                if ($request->password == $data_login->password) {
+                    $users = session(['data_login' => $data_login]);
+                    return redirect()->route('dashboard');
+                }
+                break;
+        }
+    }
+
+    public function postRegister(Request $request)
+    {
+        $login_data = new Login;
+        $validatedLogin = $request->validate([
+            'email' => 'required',
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        $hashPassword = Hash::make($request->password, [
+            'rounds' => 12,
+        ]);
+        $token = Str::random(16);
+        $level = "admin";
+        $login_data = Login::create([
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => $hashPassword,
+            'level' => $level,
+            'token' => $token,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        $login_data->save();
+        return redirect('/dashboard/login')->with('berhasil_register', 'Berhasil melakukan registrasi');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackController;
+
+Route::get('/dashboard', [BackController::class, 'index']);
