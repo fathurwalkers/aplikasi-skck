a454e64abba4489c3762bf5665e4d8f73ef5a462
Fathur Walkers <fathurwalkers44@gmail.com>
2021-07-21 23:35:40 -0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BackController;
 
+Route::get('/login', [BackController::class, 'login'])->name('login');
+Route::post('/login', [BackController::class, 'postLogin'])->name('post-login');
+Route::get('/register', [BackController::class, 'register'])->name('register');
+Route::post('/register', [BackController::class, 'postRegister'])->name('post-register');
+
 Route::get('/dashboard', [BackController::class, 'index'])->name('beranda');
