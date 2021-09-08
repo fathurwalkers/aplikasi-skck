738685b702dc6e725075b19bd103c34f6de2b0da
Fathur Walkers <fathurwalkers44@gmail.com>
2021-09-08 03:23:16 -0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BackController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/login', [BackController::class, 'login'])->name('login');
 Route::post('/login', [BackController::class, 'postLogin'])->name('post-login');
