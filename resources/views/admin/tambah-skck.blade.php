430fc8c25d5ab0e1f28ff89ff6a863ac032629c0
Fathur Walkers <fathurwalkers44@gmail.com>
2021-09-28 08:50:05 -0700

    update

--- resources/views/admin/tambah-skck.blade.php
+++ resources/views/admin/tambah-skck.blade.php
@@ -4,6 +4,31 @@
     
 @endpush
 
+{{-- // 'nama_lengkap' --}}
+{{-- // 'ttl' --}}
+{{-- // 'agama' --}}
+{{-- // 'kebangsaan' --}}
+{{-- // 'jenis_kelamin' --}}
+{{-- // 'status_kawin' --}}
+{{-- // 'pekerjaan' --}}
+{{-- // 'alamat_lengkap' --}}
+{{-- // 'no_ktp' --}}
+{{-- // 'no_passport' --}}
+{{-- // 'no_kitaskitap' --}}
+{{-- // 'no_telepon' --}}
+
+{{-- // 'status_hubungan'  Suami - Istri --}}
+{{-- // 'nama_pasangan' --}}
+{{-- // 'umur_pasangan' --}}
+{{-- // 'agama_pasangan' --}}
+{{-- // 'kebangsaan_pasangan' --}}
+{{-- // 'pekerjaan_pasangan' --}}
+{{-- // 'alamat_pasangan' --}}
+
+{{-- // 'nama_ayah' --}}
+{{-- // 'umur_ayah' --}}
+{{-- // 'agama_ayah' --}}
+
 @section('main-content')
 <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="card">
@@ -11,11 +36,67 @@
             <h4>Pembuatan SKCK</h4>
         </div>
         <div class="card-body">
-            <div class="input-group mb-3">
-                <div class="input-group-prepend">
-                  <span class="input-group-text" id="basic-addon1">@</span>
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-sm-12 col-md-12 col-lg-12">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><span style="color:red;">* </span>Nama Lengkap : </label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="masukkan Nama Lengkap anda... ">
+                            <small id="emailHelp" class="form-text text-muted">contoh : Risky Yatno</small>
+                        </div>
+                    </div>
                 </div>
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+
+                <div class="row">
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><span style="color:red;">* </span>Kebangsaan : </label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="masukkan tempat dan tanggal lahir anda... ">
+                            <small id="emailHelp" class="form-text text-muted">contoh : INDONESIA</small>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><span style="color:red;">* </span>Agama : </label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="masukkan agama anda... ">
+                            {{-- <small id="emailHelp" class="form-text text-muted"></small> --}}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><span style="color:red;">* </span>Tempat/Tanggal Lahir : </label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="masukkan tempat dan tanggal lahir anda... ">
+                            <small id="emailHelp" class="form-text text-muted">contoh : 18 Desember 1997</small>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 col-md-3 col-lg-3">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><span style="color:red;">* </span>Jenis Kelamin : </label>
+                            <select id="inputState" class="form-control">
+                                <option selected>Pilih jenis Kelamin...</option>
+                                <option>LAKI - LAKI</option>
+                                <option>PEREMPUAN</option>
+                            </select>
+                            {{-- <small id="emailHelp" class="form-text text-muted"></small> --}}
+                        </div>
+                    </div>
+                    <div class="col-sm-3 col-md-3 col-lg-3">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><span style="color:red;">* </span>Status Kawin : </label>
+                            <select id="inputState" class="form-control">
+                                <option selected>Pilih status...</option>
+                                <option>KAWIN</option>
+                                <option>TIDAK KAWIN</option>
+                            </select>
+                            {{-- <small id="emailHelp" class="form-text text-muted"></small> --}}
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
