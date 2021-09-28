6d1b7dc3f7dacbe704a8c94f9fc64052da758cfd
Fathur Walkers <fathurwalkers44@gmail.com>
2021-09-28 04:26:06 -0700

    update

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Login;
+use App\Models\Laporan;
 
 class Detail extends Model
 {
@@ -17,5 +18,10 @@ class Detail extends Model
     public function login()
     {
         return $this->hasOne(Login::class);
+    }
+
+    public function laporan()
+    {
+        return $this->hasOne(Laporan::class);
     }
 }
