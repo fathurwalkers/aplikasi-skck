4eb71fcffc7c80a99674d774b7c0e59df45fe150
Fathur Walkers <fathurwalkers44@gmail.com>
2021-09-29 08:30:37 -0700

    update

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -82,7 +82,32 @@ class BackController extends Controller
 
     public function post_tambah_skck(Request $request)
     {
-        
+        $table->string('foto');
+        $table->string('status_skck'); // Verified - Unverified 
+        $table->string('nama_lengkap');
+        $table->string('ttl');
+        $table->string('agama');
+        $table->string('kebangsaan');
+        $table->string('jenis_kelamin');
+        $table->string('status_kawin');
+        $table->string('pekerjaan');
+        $table->string('alamat_lengkap');
+        $table->string('no_ktp');
+        $table->string('no_passport')->nullable();
+        $table->string('no_kitaskitap')->nullable();
+        $table->string('no_telepon');
+
+        $table->string('status_hubungan'); // Suami - Istri
+        $table->string('nama_pasangan');
+        $table->string('umur_pasangan');
+        $table->string('agama_pasangan');
+        $table->string('kebangsaan_pasangan');
+        $table->string('pekerjaan_pasangan');
+        $table->string('alamat_pasangan');
+
+        $table->string('nama_ayah');
+        $table->string('umur_ayah');
+        $table->string('agama_ayah');
     }
 
     public function edit_skck()
