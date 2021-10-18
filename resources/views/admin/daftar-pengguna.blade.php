2c5bd7491bab7ae14cc8db7ec1ef29ce10040f7e
Fathur Walkers <fathurwalkers44@gmail.com>
2021-10-17 23:54:20 -0700

    update

--- resources/views/admin/daftar-pengguna.blade.php
+++ resources/views/admin/daftar-pengguna.blade.php
@@ -74,7 +74,7 @@
         });
     </script>
 
-    <script src=//code.jquery.com/jquery-3.5.1.slim.min.js integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin=anonymous></script>
+    {{-- <script src=//code.jquery.com/jquery-3.5.1.slim.min.js integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin=anonymous></script> --}}
 
     <script>
         
