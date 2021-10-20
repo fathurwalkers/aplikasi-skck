5da6cdece8a6c58ff3514859788d0c49656bca82
Fathur Walkers <fathurwalkers44@gmail.com>
2021-10-20 10:09:28 -0700

    update

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -13,7 +13,7 @@
                     <h4>Perpanjangan SKCK</h4>
                 </div>
                 <div class="col-sm-6 col-md-6 col-lg-6">
-                    <form action="{{ route('dashboard') }}" method="POST">
+                    <form action="{{ route('dashboard') }}" method="GET">
                         @csrf
                         <button type="submit" class="btn btn-danger float-right">Kembali</button>
                     </form>
