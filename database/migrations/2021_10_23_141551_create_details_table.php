f63c2880edaafd54f67429219c428e55f667ce94
unknown <fathurwalkers44@gmail.com>
2021-12-12 23:05:56 -0800

    update

--- database/migrations/2021_10_23_141551_create_details_table.php
+++ database/migrations/2021_10_23_141551_create_details_table.php
@@ -10,32 +10,45 @@ class CreateDetailsTable extends Migration
     {
         Schema::create('data_skck', function (Blueprint $table) {
             $table->id();
-            $table->string('foto');
-            $table->string('status_skck'); // Verified - Unverified
-            $table->string('nama_lengkap');
-            $table->string('ttl');
-            $table->string('agama');
-            $table->string('kebangsaan');
-            $table->string('jenis_kelamin');
-            $table->string('status_kawin');
-            $table->string('pekerjaan');
-            $table->string('alamat_lengkap');
-            $table->string('no_ktp');
+            $table->string('foto')->nullable();
+            $table->string('status_skck')->nullable(); // Verified - Unverified
+
+            $table->string('jenis_keperluan_pembuatan')->nullable();
+
+            $table->string('nama_lengkap')->nullable();
+
+            $table->string('ttl')->nullable();
+            $table->string('agama')->nullable();
+            $table->string('kebangsaan')->nullable();
+
+            $table->string('kabupaten')->nullable(); // Kabupaten Buton - Kabupaten Buton Selatan 
+
+            $table->string('jenis_kelamin')->nullable();
+            $table->string('status_kawin')->nullable();
+            $table->string('pekerjaan')->nullable();
+            $table->string('alamat_lengkap')->nullable();
+            $table->string('no_ktp')->nullable();
             $table->string('no_passport')->nullable();
             $table->string('no_kitaskitap')->nullable();
-            $table->string('no_telepon');
+            $table->string('no_telepon')->nullable();
 
-            $table->string('status_hubungan'); // Suami - Istri
-            $table->string('nama_pasangan');
-            $table->string('umur_pasangan');
-            $table->string('agama_pasangan');
-            $table->string('kebangsaan_pasangan');
-            $table->string('pekerjaan_pasangan');
-            $table->string('alamat_pasangan');
+            $table->text('jenis_pidana')->nullable();
 
-            $table->string('nama_ayah');
-            $table->string('umur_ayah');
-            $table->string('agama_ayah');
+            $table->string('status_hubungan')->nullable(); // Suami - Istri 
+            $table->string('nama_pasangan')->nullable();
+            $table->string('umur_pasangan')->nullable();
+            $table->string('agama_pasangan')->nullable();
+            $table->string('kebangsaan_pasangan')->nullable();
+            $table->string('pekerjaan_pasangan')->nullable();
+            $table->string('alamat_pasangan')->nullable();
+
+            $table->string('nama_ayah')->nullable();
+            $table->string('umur_ayah')->nullable();
+            $table->string('agama_ayah')->nullable();
+
+            $table->string('kebangsaan_ayah')->nullable();
+            $table->string('pekerjaan_ayah')->nullable();
+            $table->string('alamat_sekarang_ayah')->nullable();
 
             $table->unsignedBigInteger('login_id')->nullable()->default(null);
             $table->foreign('login_id')->references('id')->on('login')->onDelete('cascade');
